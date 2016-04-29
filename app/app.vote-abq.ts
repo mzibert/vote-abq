1cb778afa3bdf76ae71727a7d41d79faedfa4a66
Dylan McDonald <dmcdonald21@cnm.edu>
2016-04-28 21:46:57 -0600

    added service and AJAX caller

--- app/app.vote-abq.ts
+++ app/app.vote-abq.ts
@@ -1,8 +1,13 @@
 import {Component} from "angular2/core";
+import {VotingList} from "./voting-list";
+import {HTTP_PROVIDERS} from "angular2/http";
+import {VotingService} from "./voting.service";
 
 @Component({
 	selector: "vote-abq",
-	templateUrl: "app/templates/splash-page.html"
+	templateUrl: "app/templates/splash-page.html",
+	directives: [VotingList],
+	providers: [HTTP_PROVIDERS, VotingService]
 })
 
 export class VoteAbq {}
