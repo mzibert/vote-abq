d0ee64bc17db1b767566913c0d1dce143a09a81f
Dylan McDonald <dmcdonald21@cnm.edu>
2016-04-28 21:47:40 -0600

    added AJAX caller

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,6 @@
 import {bootstrap} from "angular2/platform/browser";
+import {HTTP_PROVIDERS} from "angular2/http";
+import "rxjs/Rx";
 import {VoteAbq} from "./app.vote-abq";
 
 bootstrap(VoteAbq);
