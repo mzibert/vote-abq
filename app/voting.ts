b4ab26b8679b9c376a4a484bc7030f2a8b13c124
Dylan McDonald <dmcdonald21@cnm.edu>
2016-04-28 20:39:37 -0600

    added voting class

---
+++ app/voting.ts
@@ -0,0 +1,11 @@
+export class Voting {
+	constructor(
+		public id: number,
+		public name: string,
+		public address: string,
+		public zip: string,
+		public description: string,
+		public lat: number,
+		public long: number
+	) {}
+}
